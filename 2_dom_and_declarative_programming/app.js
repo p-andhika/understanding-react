78ebe9b33f8e8eb8febb34ea0b733852bb9336e4
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-04-24 15:09:33 +0700

    refactor: embrace declarative paradigm for counter logic

--- 2_dom_and_declarative_programming/app.js
+++ 2_dom_and_declarative_programming/app.js
@@ -9,13 +9,36 @@ setTimeout(() => {
 
 // Declarative & Imperative
 // ========================
+const countElement = document.getElementById("count");
+
+const app = {
+  getCount: () => {
+    return Number(countElement.textContent);
+  },
+  setCount: (val) => {
+    countElement.textContent = val;
+  },
+};
 
 // Imperative
 // Telling the browser what to do
-const countElement = document.getElementById("count");
+// ==============================
+// const countElement = document.getElementById("count");
+// function setCount() {
+//   let count = Number(countElement.textContent);
+//   count = count + 1;
+//   countElement.textContent = count;
+// }
 
+// Declarative
+// Telling the browser what we want,
+// no detail instruction given like what we've done in imperative
+// ==============================================================
 function setCount() {
-  let count = Number(countElement.textContent);
-  count = count + 1;
-  countElement.textContent = count;
+  let count = app.getCount();
+  if (count >= 5) {
+    app.setCount(0);
+  } else {
+    app.setCount(count + 1);
+  }
 }
