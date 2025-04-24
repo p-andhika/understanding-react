db8b7e5ebfac8757eb8ec8289bf862ef2744c936
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-04-24 15:03:09 +0700

    feat: add interactive counter to demonstrate imperative DOM

--- 2_dom_and_declarative_programming/app.js
+++ 2_dom_and_declarative_programming/app.js
@@ -6,3 +6,16 @@ newListElement.textContent = "Item 3";
 setTimeout(() => {
   listElement.appendChild(newListElement);
 }, 1000);
+
+// Declarative & Imperative
+// ========================
+
+// Imperative
+// Telling the browser what to do
+const countElement = document.getElementById("count");
+
+function setCount() {
+  let count = Number(countElement.textContent);
+  count = count + 1;
+  countElement.textContent = count;
+}
