6402ad896cb68d617c824098e0fb404c5e289759
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-04-25 13:35:17 +0700

    feat: embrace React for dynamic UI rendering

--- 3_react_element/app.js
+++ 3_react_element/app.js
@@ -1,50 +1,64 @@
+// React
+// =====
+const rootNode = document.getElementById("app");
+const root = ReactDOM.createRoot(rootNode);
+root.render(React.createElement(App));
+
+console.log(React);
+
+function App() {
+  return React.createElement("button", null, "Click me");
+}
+
+console.log(App());
+
 // Recursion
 // =========
 
-function countDown(num) {
-  console.log(num);
-
-  if (num === 0) return;
-
-  countDown(num - 1);
-}
+// function countDown(num) {
+//   console.log(num);
+//
+//   if (num === 0) return;
+//
+//   countDown(num - 1);
+// }
 
 // countDown(5);
 
 // POJOs
 // Plain Old JavaScript Object(s)
 // =============================
-const markup = {
-  type: "article",
-  children: [
-    {
-      type: "h1",
-      children: [
-        {
-          type: "text",
-          value: "Hello World!",
-        },
-      ],
-    },
-  ],
-};
-
-const main = document.getElementById("app");
-
-function addElements(pojoElement, parentDOMNode) {
-  const newDOMNode =
-    pojoElement.type === "text"
-      ? document.createTextNode(pojoElement.value)
-      : document.createElement(pojoElement.type);
-
-  if (pojoElement.children) {
-    pojoElement.children.forEach((child) => {
-      addElements(child, newDOMNode);
-    });
-  }
-
-  console.log(parentDOMNode);
-  parentDOMNode.appendChild(newDOMNode);
-}
-
-addElements(markup, main);
+// const markup = {
+//   type: "article",
+//   children: [
+//     {
+//       type: "h1",
+//       children: [
+//         {
+//           type: "text",
+//           value: "Hello World!",
+//         },
+//       ],
+//     },
+//   ],
+// };
+//
+// const main = document.getElementById("app");
+//
+// function addElements(pojoElement, parentDOMNode) {
+//   const newDOMNode =
+//     pojoElement.type === "text"
+//       ? document.createTextNode(pojoElement.value)
+//       : document.createElement(pojoElement.type);
+//
+//   if (pojoElement.children) {
+//     pojoElement.children.forEach((child) => {
+//       addElements(child, newDOMNode);
+//     });
+//   }
+//
+//   console.log(parentDOMNode);
+//   parentDOMNode.appendChild(newDOMNode);
+// }
+//
+// addElements(markup, main);
